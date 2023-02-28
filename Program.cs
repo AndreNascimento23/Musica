3371e29bc565befb83a24b9aaafbb256e4ed0983
André Nascimento <andre4.20nascimento@outlook.com>
2023-02-28 17:12:14 -0300

    Exercicio letra de musica concluida

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine();
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Aonde Quer Que Eu Vá - Paralamas do Sucesso");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Olhos fechados");
+Console.WriteLine("Pra te encontrar");
+Console.WriteLine("Não estou ao seu lado");
+Console.WriteLine("Mas posso sonhar");
+Console.WriteLine("Aonde quer que eu vá");
+Console.WriteLine("Levo você no olhar");
+Console.WriteLine("Aonde quer que eu vá");
+Console.WriteLine("Aonde quer que eu vá");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor. Red;
+Console.WriteLine("Não sei bem certo");
+Console.WriteLine("Se é só ilusão");
+Console.WriteLine("Se é você já perto");
+Console.WriteLine("Se é intuição");
+Console.WriteLine("E aonde quer que eu vá");
+Console.WriteLine("Levo você no olhar");
+Console.WriteLine("Aonde quer que eu vá");
+Console.WriteLine("Aonde quer que eu vá");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Longe daqui");
+Console.WriteLine("Longe de tudo");
+Console.WriteLine("Meus sonhos vão te buscar");
+Console.WriteLine("Volta pra mim");
+Console.WriteLine("Vem pro meu mundo");
+Console.WriteLine("Eu sempre vou te esperar");
+Console.WriteLine("Larará! Lararára!");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor. Red;
+Console.WriteLine("Não sei bem certo");
+Console.WriteLine("Se é só ilusão");
+Console.WriteLine("Se é você já perto");
+Console.WriteLine("Se é intuição");
+Console.WriteLine("E aonde quer que eu vá");
+Console.WriteLine("Levo você no olhar");
+Console.WriteLine("Aonde quer que eu vá");
+Console.WriteLine("Aonde quer que eu vá");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Lá! Larará! Larará!");
+Console.WriteLine("Lá! Larará! Larará!");
+Console.WriteLine("Aonde quer que eu vá");
+Console.WriteLine("Lá! Larará! Larará!");
+Console.WriteLine("Lá! Larará! Larará!");
+Console.WriteLine("Lá! Larará! Larará!");
+Console.WriteLine("Aonde quer que eu vá");
+Console.WriteLine();
